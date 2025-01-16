adc3166474929b12091d197473aff3a513f7169a
eladrab1106 <eladrabinovitch1106@cs.colman.ac.il>
2025-01-16 09:38:09 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,21 +10,19 @@ import authRoutes from "./routes/authRoutes";
 import { Server } from "socket.io";
 import { chatSocket } from "./socket/chatSocket";
 
-
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
 mongoose
-    .connect(process.env.MONGO_URI as string)
-    .then(() => {
-        console.log("Connected to the MongoDB database successfully.");
-    })
-    .catch((error) => {
-        console.error("Error connecting to the database:", error.message);
-    });
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("Connected to the MongoDB database successfully.");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error.message);
+  });
 
-app.use(cors({ origin: "http://localhost:5173" })); 
-
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -33,14 +31,14 @@ app.use("/comment", commentsRoutes);
 app.use("/auth", authRoutes);
 
 const server = app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 const io = new Server(server, {
-    cors: {
-        origin: "*", 
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 chatSocket(io);
