2ca73713bc47ef362c3cb80472c58e940b2bd2da
eladrab1106 <eladrabinovitch1106@cs.colman.ac.il>
2025-01-16 02:43:12 +0200

    get comments by post id

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import BaseController from "../controllers/comment";
 import { authMiddleware } from "../controllers/auth_controller";
+import { getCommentsByPostId } from "../controllers/comment";
 
 const router = express.Router();
 
 router.get("/", BaseController.getAll);
 router.post("/",authMiddleware, BaseController.createItem);
-router.get("/:postId", BaseController.getAll);
+router.get("/:postId", getCommentsByPostId);
 router.put("/:id",authMiddleware, BaseController.updateItem);
 router.delete("/:id",authMiddleware, BaseController.deleteItem);
 
